5f55866bd43124d64c88ccedb9bf93e0048d4196
Pablo Eliezer <pablo.eliezer1990@gmail.com>
2020-09-15 14:55:19 -0500

    problema al verificar token

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -8,9 +8,7 @@ let verificaToken = (req, res, next) => {
         if (err) {
             return res.status(401).json({
                 ok: false,
-                err: {
-                    message: 'Token no válido'
-                }
+                err
             });
         }
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -37,7 +37,7 @@ app.get('/usuario', verificaToken, (req, res) => {
         });
 });
 
-app.post('/usuario', [verificaToken, verificaAAdminRole], function(req, res) {
+app.post('/usuario', function(req, res) {
 
     let body = req.body;
     let usuario = new Usuario({
