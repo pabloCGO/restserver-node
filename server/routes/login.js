730976af9c524c3690eb8ce3e8ac8dd52296ae31
Pablo Eliezer <pablo.eliezer1990@gmail.com>
2020-09-15 15:13:39 -0500

    fix verificar token

--- server/routes/login.js
+++ server/routes/login.js
@@ -36,7 +36,7 @@ app.post('/login', (req, res) => {
 
         let token = jwt.sign({
             usuario: usuarioDB
-        }, 'hola-que-hace-jwt', { expiresIn: process.env.CADUCIDAD_TOKEN });
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
 
         res.json({
